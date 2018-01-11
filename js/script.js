c9834f8b984d0ac0b3beb4fffeee389e2ab56c7c
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-11 16:17:53 +0530

    Creating JavaScript Objects : The Dynamic Nature of JavaScript!

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+'use strict';
+
+var cat = {
+  name: 'Fluffy',
+  color: 'White',
+  speak: function () {
+    console.log('Meeoow');
+  }
+};
+cat.age = 3;
+
+cat.speak();
+
+console.log(cat);
