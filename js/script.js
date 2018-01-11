641fdd5adea542e089d3448bde278c395a841131
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-11 20:04:25 +0530

    Intro to prototype, Part1

--- js/script.js
+++ js/script.js
@@ -5,31 +5,12 @@
 /* eslint-disable */
 'use strict';
 
-Object.defineProperty(Array.prototype, 'first', {
-  get: function () {
-    return this[0];
-  }
-});
+var myFunction = function () {}
+console.log(myFunction.prototype);
+console.log(myFunction.prototype.__proto__);
 
-Object.defineProperty(Array.prototype, 'second', {
-  get: function () {
-    return this[1];
-  }
-});
+var cat = {
+  name: 'Fluffy',
+};
 
-Object.defineProperty(Array.prototype, 'last', {
-  get: function () {
-    return this[this.length - 1];
-  }
-});
-
-// var colors = new Array('red', 'blue', 'green');
-// or, Syntactic sugar below:
-var colors = ['red', 'blue', 'green'];
-console.log(colors.first, colors.second, colors.last);
-
-// var numbers = new Array('one', 'two', 'three');
-// or, Syntactic sugar below:
-var numbers = ['one', 'two', 'three']
-console.log(numbers.first, numbers.second, numbers.last);
-
+console.log(cat.__proto__);
