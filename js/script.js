3236962bbb22ff0c8ab800c3393fcd38c2463262
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-12 15:27:11 +0530

    Multiple levels of inheritance!

--- js/script.js
+++ js/script.js
@@ -13,21 +13,8 @@ function Cat(name, color) {
 Cat.prototype.age = 4;
 
 var fluffy = new Cat('Fluffy', 'White');
-var muffin = new Cat('Muffin', 'Brown');
-
-Cat.prototype = {
-  age: 5,
-};
-
-var snowbell = new Cat('Snowbell', 'White');
 
 console.log(fluffy);
-console.log(fluffy.age);
-
-console.log(muffin);
-console.log(muffin.age);
-
-console.log(snowbell);
-console.log(snowbell.age)
-
-console.log(Cat.prototype.age);
+console.log(fluffy.__proto__);
+console.log(fluffy.__proto__.__proto__);
+console.log(fluffy.__proto__.__proto__.__proto__);
