9386db618a472c979c4aa24abb31eaf260b7147c
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-12 17:27:25 +0530

    Creating own prototypal inheritance chain!

--- js/script.js
+++ js/script.js
@@ -5,16 +5,25 @@
 /* eslint-disable */
 'use strict';
 
+function Animal(voice) {
+  this.voice = voice || 'grunt';
+}
+
+Animal.prototype.speak = function () {
+  console.log(this.voice);
+}
+
 function Cat(name, color) {
+  Animal.call(this, 'Meow');
   this.name = name;
   this.color = color;
 }
 
-Cat.prototype.age = 4;
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
 
 var fluffy = new Cat('Fluffy', 'White');
-
 console.log(fluffy);
-console.log(fluffy.__proto__);
-console.log(fluffy.__proto__.__proto__);
-console.log(fluffy.__proto__.__proto__.__proto__);
+
+console.log(fluffy instanceof Cat);
+console.log(fluffy instanceof Animal);
